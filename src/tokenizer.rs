50bc0fb05306852a50c8ff7bd1aaec1c9ec2d196
AmaranthineCodices <31936135+AmaranthineCodices@users.noreply.github.com>
2018-04-04 15:51:36 -0400

    Track line and column of tokens in tokenizer (#2)

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -13,7 +13,7 @@ pub enum TokenKind<'a> {
     CloseParen,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Token<'a> {
     /// Any whitespace before the token
     pub whitespace: &'a str,
@@ -21,13 +21,20 @@ pub struct Token<'a> {
     /// Details about the Token itself
     pub kind: TokenKind<'a>,
 
-    // TODO: The line/column that the token is on?
+    pub line: usize,
+    pub column: usize,
     // TODO: A slice from the source indicating what the token came from
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum TokenizeError<'a> {
     UnknownSequence(&'a str),
+}
+
+struct TryAdvanceResult<'a> {
+    new_source: &'a str,
+    eaten_str: &'a str,
+    matched_kind: TokenKind<'a>,
 }
 
 lazy_static! {
@@ -43,11 +50,12 @@ lazy_static! {
     static ref PATTERN_CLOSE_PAREN: Regex = Regex::new(r"^(\))").unwrap();
 
     static ref PATTERN_WHITESPACE: Regex = Regex::new(r"^\s+").unwrap();
+    static ref PATTERN_CHARS_AFTER_NEWLINE: Regex = Regex::new(r"\n([^\n]+)$").unwrap();
 }
 
 /// Tries to matches the given pattern against the string slice.
 /// If it does, the 'tokenizer' fn is invokved to turn the result into a token.
-fn try_advance<'a, F>(source: &'a str, pattern: &Regex, tokenizer: F) -> Option<(&'a str, TokenKind<'a>)>
+fn try_advance<'a, F>(source: &'a str, pattern: &Regex, tokenizer: F) -> Option<TryAdvanceResult<'a>>
 where
     F: Fn(&'a str) -> TokenKind<'a>,
 {
@@ -56,7 +64,12 @@ where
         // have noncapturing groups that need to be ignored!
         let capture = captures.get(1).unwrap();
         let contents = capture.as_str();
-        Some((&source[capture.end()..], tokenizer(contents)))
+
+        Some(TryAdvanceResult {
+            new_source: &source[capture.end()..],
+            eaten_str: contents,
+            matched_kind: tokenizer(contents),
+        })
     } else {
         None
     }
@@ -72,16 +85,47 @@ fn eat<'a>(source: &'a str, pattern: &Regex) -> (&'a str, Option<&'a str>) {
     }
 }
 
+fn get_new_position<'a>(eaten_str: &'a str, current_line: usize, current_column: usize) -> (usize, usize) {
+    let lines_eaten = eaten_str.matches("\n").count();
+
+    let column = if lines_eaten > 0 {
+        // If there was a newline we're on a totally different column
+
+        if let Some(captures) = PATTERN_CHARS_AFTER_NEWLINE.captures(eaten_str) {
+            // If there's some characters after the newline, count them!
+            // Add 1 so we start at a column of 1
+            captures.get(1).unwrap().as_str().len() + 1
+        }
+        else {
+            // Otherwise, just restart at 1.
+            1
+        }
+    }
+    else {
+        // Otherwise we can just increment the current column by the length of the eaten chars
+        current_column + eaten_str.len()
+    };
+
+    // We return the new line count, not the delta line count
+    (current_line + lines_eaten, column)
+}
+
 // TODO: Change to returning iterator?
 pub fn tokenize<'a>(source: &'a str) -> Result<Vec<Token<'a>>, TokenizeError<'a>> {
     let mut tokens = Vec::new();
     let mut current = source;
+    let mut current_line = 1;
+    let mut current_column = 1;
 
     loop {
         let (next_current, matched_whitespace) = eat(current, &PATTERN_WHITESPACE);
         let whitespace = matched_whitespace.unwrap_or("");
 
         current = next_current;
+
+        let (new_line, new_column) = get_new_position(whitespace, current_line, current_column);
+        current_line = new_line;
+        current_column = new_column;
 
         let result = try_advance(current, &PATTERN_IDENTIFIER, |s| {
                 if KEYWORDS.contains(s) {
@@ -96,13 +140,19 @@ pub fn tokenize<'a>(source: &'a str) -> Result<Vec<Token<'a>>, TokenizeError<'a>
             .or_else(|| try_advance(current, &PATTERN_CLOSE_PAREN, |_| TokenKind::CloseParen));
 
         match result {
-            Some((next_current, token_kind)) => {
-                current = next_current;
+            Some(result) => {
+                current = result.new_source;
 
                 tokens.push(Token {
                     whitespace,
-                    kind: token_kind,
+                    kind: result.matched_kind,
+                    line: current_line,
+                    column: current_column,
                 });
+
+                let (new_line, new_column) = get_new_position(result.eaten_str, current_line, current_column);
+                current_line = new_line;
+                current_column = new_column;
             }
             None => break,
         }
@@ -151,5 +201,50 @@ mod tests {
         let first_token = tokenized[0];
 
         assert_eq!(first_token.whitespace, "");
+    }
+
+    #[test]
+    fn get_new_line_info() {
+        let (new_line, new_column) = get_new_position("test", 1, 1);
+        assert_eq!(new_line, 1);
+        assert_eq!(new_column, 5);
+
+        let (new_line, new_column) = get_new_position("testy\ntest", 1, 1);
+        assert_eq!(new_line, 2);
+        assert_eq!(new_column, 5);
+    }
+
+    #[test]
+    fn source_tracking() {
+        let input = "local
+                    test foo
+                    bar";
+        let tokenized = tokenize(input).unwrap();
+        assert_eq!(tokenized, vec![
+            Token {
+                kind: TokenKind::Keyword("local"),
+                whitespace: "",
+                line: 1,
+                column: 1,
+            },
+            Token {
+                kind: TokenKind::Identifier("test"),
+                whitespace: "\n                    ",
+                line: 2,
+                column: 21,
+            },
+            Token {
+                kind: TokenKind::Identifier("foo"),
+                whitespace: " ",
+                line: 2,
+                column: 26,
+            },
+            Token {
+                kind: TokenKind::Identifier("bar"),
+                whitespace: "\n                    ",
+                line: 3,
+                column: 21,
+            }
+        ]);
     }
 }
