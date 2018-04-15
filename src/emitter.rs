db63eafee31d2cea0446f891c8e45cad015eb523
Lucien Greathouse <me@lpghatguy.com>
2018-04-15 00:14:07 -0700

    Stub out emitter

--- src/emitter.rs
+++ src/emitter.rs
@@ -1,5 +1,56 @@
-use ast::Chunk;
+use std::fmt::{self, Write};
 
-pub fn emit_chunk<'a>(chunk: Chunk<'a>) -> &'a str {
-    ""
+use ast::{
+    Chunk,
+    Statement,
+    LocalAssignment,
+    FunctionCall,
+    NumericFor,
+};
+
+pub fn emit_chunk<'a>(w: &mut Write, chunk: &Chunk<'a>) -> fmt::Result {
+    for statement in &chunk.statements {
+        emit_statement(w, statement)?;
+    }
+
+    Ok(())
+}
+
+fn emit_statement<'a>(w: &mut Write, statement: &Statement<'a>) -> fmt::Result {
+    match statement {
+        &Statement::LocalAssignment(ref value) => emit_local_assignment(w, value)?,
+        &Statement::FunctionCall(ref value) => emit_function_call(w, value)?,
+        &Statement::NumericFor(ref value) => emit_numeric_for(w, value)?,
+    }
+
+    Ok(())
+}
+
+fn emit_local_assignment<'a>(w: &mut Write, assignment: &LocalAssignment<'a>) -> fmt::Result {
+    Ok(())
+}
+
+fn emit_function_call<'a>(w: &mut Write, function_call: &FunctionCall<'a>) -> fmt::Result {
+    Ok(())
+}
+
+fn emit_numeric_for<'a>(w: &mut Write, numeric_for: &NumericFor<'a>) -> fmt::Result {
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn emit_empty_chunk() {
+        let chunk = Chunk {
+            statements: Vec::new(),
+        };
+
+        let mut output = String::new();
+        emit_chunk(&mut output, &chunk);
+
+        assert_eq!(&output, "");
+    }
 }
