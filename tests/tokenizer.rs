616975f6f3adfab0a9322c3526aa8894f0dbe710
Lucien Greathouse <me@lpghatguy.com>
2018-04-17 21:21:32 -0700

    Implement tokenizer tests, including one example test

---
+++ tests/tokenizer.rs
@@ -0,0 +1,111 @@
+extern crate lua_parser;
+extern crate serde_json;
+
+use std::fs::{File, read_dir};
+use std::io::{Read, Write};
+
+use lua_parser::{tokenize, parse_from_tokens, Token, ast::Chunk};
+
+#[test]
+fn tokenizer() {
+    for entry in read_dir("parse_examples/tokenizer_tests").unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path().to_str().unwrap().to_string();
+
+        if !path.ends_with(".lua") {
+            continue;
+        }
+
+        let expected_tokens_path = path.replace(".lua", ".tokens.json");
+        let expected_ast_path = path.replace(".lua", ".ast.json");
+
+        let contents = {
+            let mut file = File::open(entry.path())
+                .expect("Unable to open file!");
+
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)
+                .expect("Unable to read from file!");
+
+            contents
+        };
+
+        let mut expected_token_contents = String::new();
+        let expected_tokens = match File::open(&expected_tokens_path) {
+            Ok(mut file) => {
+                file.read_to_string(&mut expected_token_contents)
+                    .expect("Unable to read from file!");
+
+                let result: Vec<Token> = serde_json::from_str(&expected_token_contents)
+                    .expect("Unable to deserialize JSON!");
+
+                Some(result)
+            },
+            Err(_) => None,
+        };
+
+        let mut expected_ast_contents = String::new();
+        let expected_ast = match File::open(&expected_ast_path) {
+            Ok(mut file) => {
+                file.read_to_string(&mut expected_ast_contents)
+                    .expect("Unable to read from file!");
+
+                let result: Chunk = serde_json::from_str(&expected_ast_contents)
+                    .expect("Unable to deserialize JSON!");
+
+                Some(result)
+            },
+            Err(_) => None,
+        };
+
+        let tokens = match tokenize(&contents) {
+            Ok(tokens) => tokens,
+            Err(err) => {
+                panic!("Failed to tokenize file {}: {:?}", entry.path().display(), err);
+            },
+        };
+
+        let ast = match parse_from_tokens(&tokens) {
+            Ok(ast) => ast,
+            Err(err) => {
+                panic!("Failed to parse file {}: {:?}", entry.path().display(), err);
+            },
+        };
+
+        match expected_tokens {
+            Some(expected_tokens) => {
+                println!("Checking against existing expectated tokens file {}", expected_tokens_path);
+
+                assert_eq!(tokens, expected_tokens)
+            },
+            None => {
+                println!("Creating expectated tokens file {}", expected_tokens_path);
+
+                let mut file = File::create(&expected_tokens_path)
+                    .expect("Unable to create file!");
+
+                let contents = serde_json::to_string_pretty(&tokens).unwrap();
+
+                file.write_all(contents.as_bytes()).unwrap();
+            }
+        }
+
+        match expected_ast {
+            Some(expected_ast) => {
+                println!("Checking against existing expectated AST file {}", expected_ast_path);
+
+                assert_eq!(ast, expected_ast)
+            },
+            None => {
+                println!("Creating expectated AST file {}", expected_ast_path);
+
+                let mut file = File::create(&expected_ast_path)
+                    .expect("Unable to create file!");
+
+                let contents = serde_json::to_string_pretty(&ast).unwrap();
+
+                file.write_all(contents.as_bytes()).unwrap();
+            }
+        }
+    }
+}
